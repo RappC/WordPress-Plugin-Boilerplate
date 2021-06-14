2f2aff6c8b38fa27dbaf65830126a0305065166b
Christian Rapp <christian.rapp@msg.group>
2021-06-14 16:56:37 +0200

    test

--- category-by-membership/public/class-category-by-membership-public.php
+++ category-by-membership/public/class-category-by-membership-public.php
@@ -23,159 +23,163 @@
 class Category_By_Membership_Public
 {
 
-	/**
-	 * The ID of this plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 * @var      string    $category_by_membership    The ID of this plugin.
-	 */
-	private $category_by_membership;
+  /**
+   * The ID of this plugin.
+   *
+   * @since    1.0.0
+   * @access   private
+   * @var      string    $category_by_membership    The ID of this plugin.
+   */
+  private $category_by_membership;
 
-	/**
-	 * The version of this plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 * @var      string    $version    The current version of this plugin.
-	 */
-	private $version;
+  /**
+   * The version of this plugin.
+   *
+   * @since    1.0.0
+   * @access   private
+   * @var      string    $version    The current version of this plugin.
+   */
+  private $version;
 
-	/**
-	 * Initialize the class and set its properties.
-	 *
-	 * @since    1.0.0
-	 * @param      string    $category_by_membership       The name of the plugin.
-	 * @param      string    $version    The version of this plugin.
-	 */
-	public function __construct($category_by_membership, $version)
-	{
+  /**
+   * Initialize the class and set its properties.
+   *
+   * @since    1.0.0
+   * @param      string    $category_by_membership       The name of the plugin.
+   * @param      string    $version    The version of this plugin.
+   */
+  public function __construct($category_by_membership, $version)
+  {
 
-		$this->category_by_membership = $category_by_membership;
-		$this->version = $version;
-	}
+    $this->category_by_membership = $category_by_membership;
+    $this->version = $version;
+  }
 
-	/**
-	 * Register the stylesheets for the public-facing side of the site.
-	 *
-	 * @since    1.0.0
-	 */
-	public function enqueue_styles()
-	{
+  /**
+   * Register the stylesheets for the public-facing side of the site.
+   *
+   * @since    1.0.0
+   */
+  public function enqueue_styles()
+  {
 
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in Category_By_Membership_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The Category_By_Membership_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
+    /**
+     * This function is provided for demonstration purposes only.
+     *
+     * An instance of this class should be passed to the run() function
+     * defined in Category_By_Membership_Loader as all of the hooks are defined
+     * in that particular class.
+     *
+     * The Category_By_Membership_Loader will then create the relationship
+     * between the defined hooks and the functions defined in this
+     * class.
+     */
 
-		wp_enqueue_style($this->category_by_membership, plugin_dir_url(__FILE__) . 'css/category-by-membership-public.css', array(), $this->version, 'all');
-	}
+    wp_enqueue_style($this->category_by_membership, plugin_dir_url(__FILE__) . 'css/category-by-membership-public.css', array(), $this->version, 'all');
+  }
 
-	/**
-	 * Register the JavaScript for the public-facing side of the site.
-	 *
-	 * @since    1.0.0
-	 */
-	public function enqueue_scripts()
-	{
+  /**
+   * Register the JavaScript for the public-facing side of the site.
+   *
+   * @since    1.0.0
+   */
+  public function enqueue_scripts()
+  {
 
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in Category_By_Membership_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The Category_By_Membership_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
+    /**
+     * This function is provided for demonstration purposes only.
+     *
+     * An instance of this class should be passed to the run() function
+     * defined in Category_By_Membership_Loader as all of the hooks are defined
+     * in that particular class.
+     *
+     * The Category_By_Membership_Loader will then create the relationship
+     * between the defined hooks and the functions defined in this
+     * class.
+     */
 
-		wp_enqueue_script($this->category_by_membership, plugin_dir_url(__FILE__) . 'js/category-by-membership-public.js', array('jquery'), $this->version, false);
-	}
+    wp_enqueue_script($this->category_by_membership, plugin_dir_url(__FILE__) . 'js/category-by-membership-public.js', array('jquery'), $this->version, false);
+  }
 
+  private function getMembership()
+  {
 
-	/**
-	 * Function to retrieve the correct category from post's customfield by memberpress membership and user meta data
-	 */
-	public function getCategoryForMembership()
-	{
+    $membership = 'cat_main_public';
 
-		$membership = 'cat_main_public';
+    if (class_exists('MeprUtils')) {
+      $user = MeprUtils::get_currentuserinfo();
 
-		if (class_exists('MeprUtils')) {
-			$user = MeprUtils::get_currentuserinfo();
+      if ($user !== false && isset($user->ID)) {
+        //Returns an array of Membership ID's that the current user is active on
+        //Can also use 'products' or 'transactions' as the argument type
+        $active_prodcuts = $user->active_product_subscriptions('ids');
 
-			if ($user !== false && isset($user->ID)) {
-				//Returns an array of Membership ID's that the current user is active on
-				//Can also use 'products' or 'transactions' as the argument type
-				$active_prodcuts = $user->active_product_subscriptions('ids');
+        if (!empty($active_prodcuts)) {
+          $membership_id = $active_prodcuts[0];
+          if ($membership_id == 8535) {
+            $membership = 'cat_main_independent_ars';
+          } else if ($membership_id == 6738) {
+            $membership = 'cat_main_independent_cs';
+            //      } else if ($membership_id == 8538) {
+            //        $membership = 'cat_sub_owner_cam';
+            //      } else if ($membership_id == 8540) {
+            //        $membership = 'cat_sub_owner_sd';
+            //      } else if ($membership_id == 8542) {
+            //        $membership = 'cat_sub_pilot_owner';
+          } else if ($membership_id == 6732) {
+            $membership = 'cat_main_pilot_operators';
 
-				if (!empty($active_prodcuts)) {
-					$membership_id = $active_prodcuts[0];
-					if ($membership_id == 8535) {
-						$membership = 'cat_main_independent_ars';
-					} else if ($membership_id == 6738) {
-						$membership = 'cat_main_independent_cs';
-						//      } else if ($membership_id == 8538) {
-						//        $membership = 'cat_sub_owner_cam';
-						//      } else if ($membership_id == 8540) {
-						//        $membership = 'cat_sub_owner_sd';
-						//      } else if ($membership_id == 8542) {
-						//        $membership = 'cat_sub_pilot_owner';
-					} else if ($membership_id == 6732) {
-						$membership = 'cat_main_pilot_operators';
+            // returns either 'on' if set, otherwise it is an empty string
+            $isOwnerCam = get_user_meta($user->ID, 'mepr_owner_cam', true);
+            $isOwnerSd = get_user_meta($user->ID, 'mepr_owner_sd', true);
+            $isPilotOwner = get_user_meta($user->ID, 'mepr_pilot_owner', true);
 
-						// returns either 'on' if set, otherwise it is an empty string
-						$isOwnerCam = get_user_meta($user->ID, 'mepr_owner_cam', true);
-						$isOwnerSd = get_user_meta($user->ID, 'mepr_owner_sd', true);
-						$isPilotOwner = get_user_meta($user->ID, 'mepr_pilot_owner', true);
+            if ($isOwnerCam == 'on') {
+              $membership = 'cat_sub_owner_cam';
+            }
 
-						if ($isOwnerCam == 'on') {
-							$membership = 'cat_sub_owner_cam';
-						}
+            if ($isOwnerSd == 'on') {
+              $membership = 'cat_sub_owner_sd';
+            }
 
-						if ($isOwnerSd == 'on') {
-							$membership = 'cat_sub_owner_sd';
-						}
+            if ($isPilotOwner == 'on') {
+              $membership = 'cat_sub_pilot_owner';
+            }
+          } else if ($membership_id == 8600) {
+            $membership = 'cat_main_public';
+          } else {
+            $membership = 'cat_main_public';
+          }
+        }
+      }
 
-						if ($isPilotOwner == 'on') {
-							$membership = 'cat_sub_pilot_owner';
-						}
-					} else if ($membership_id == 8600) {
-						$membership = 'cat_main_public';
-					} else {
-						$membership = 'cat_main_public';
-					}
-				}
-			}
-		}
+      return $membership;
+    }
+  }
 
-		$customFieldValues = get_post_custom_values($membership);
-		$realValue = 'unknown';
+  /**
+   * Function to retrieve the correct category from post's customfield by memberpress membership and user meta data
+   */
+  public function getCategoryForMembership()
+  {
+    $category = "unknown";
+    $membership = $this->getMembership();
 
-		if (!empty($customFieldValues)) {
-			$shortValue = $customFieldValues[0];
+    $customFieldValues = get_post_custom_values($membership);
+    
 
-			if ($shortValue == 'h') {
-				$realValue = 'hint';
-			} else if ($shortValue == 'm') {
-				$realValue = 'meaning';
-			} else if ($shortValue == 'c') {
-				$realValue = 'compliance';
-			} else {
-				$realValue = '';
-			}
-			return '<div class="ar-elementor-ribbon ar-elementor-ribbon-right"><div class="ar-elementor-ribbon-inner">' + $realValue + '</div>  </div>';
-		}
+    if (!empty($customFieldValues)) {
+      $shortValue = $customFieldValues[0];
 
-		return 'halligalli';
-	}
+      if ($shortValue == 'h') {
+        $category = 'hint';
+      } else if ($shortValue == 'm') {
+        $category = 'meaning';
+      } else if ($shortValue == 'c') {
+        $category = 'compliance';
+      }
+    }
+  
+    return '<div class="ar-elementor-ribbon ar-elementor-ribbon-right"><div class="ar-elementor-ribbon-inner">' + $category + '</div>  </div>';
+  }
 }
